225b8e19aaaf594083c6de7c659fd0c01d73275b
Vikas Jyani <vickbishnoi@gmail.com>
2021-12-19 18:22:20 +0530

    added html, css and js files

---
+++ app.js
@@ -0,0 +1,20 @@
+// function saveLead() {
+//     console.log("Button clicked!")
+// } // old and obsolete technique
+
+// Technique that Proffesionals use
+
+// let inputBtn = document.getElementById("input-btn")
+
+// inputBtn.addEventListener("click", function() {
+//     console.log("Button clicked")
+// })
+
+
+
+let inputBtn = document.getElementById("input-btn")
+
+inputBtn.addEventListener("click", function() {
+    console.log("Button clicked!")
+})
+
